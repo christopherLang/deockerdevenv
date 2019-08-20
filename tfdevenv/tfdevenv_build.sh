91eb4781da8edb9ca2ae60c59a6a582b5d6be140
Christopher Lang <chlang206@gmail.com>
2019-08-19 22:34:17 -0400

    Developing tensorflor devenv

--- tfdevenv/tfdevenv_build.sh
+++ tfdevenv/tfdevenv_build.sh
@@ -2,6 +2,7 @@
 # $IMAGE_NAME var is injected into the build so the tag is correct.
 IMAGE_NAME=chlangdevenv/tensorflow:0.0.0.9999
 
-docker build --build-arg VCS_REF=`git rev-parse — short HEAD` \
+
+docker build --build-arg VCS_REF=`git rev-parse HEAD` \
   --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` \
   -t $IMAGE_NAME .
